36a7249660b38a50d01878a52efa74fba7ebcdd0
Phillip <phillip7.et@gmail.com>
2020-09-17 22:36:28 +0700

    add script

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express'),
   app = express(),
   port = 3000,
   cors = require('cors'),
-  log= console.log
+  log = console.log,
+  spawn = require('child_process').spawn;
 
 app.use(cors());
 
@@ -10,13 +11,13 @@ app.get('/', (_, res) => {
   res.send('Welcome Remote Cli');
 });
 app.get('/remote/:serverIp', (req, res) => {
-  let serverIp = req.params.serverIp,
-    spawn = require('child_process').spawn;
+  let serverIp = req.params.serverIp;
   log(`/remote/:${serverIp}`);
   try {
     spawn('C:\\Windows\\System32\\mstsc.exe', ['/v:' + serverIp]);
     res.send({ success: true });
   } catch (error) {
+    log(error)
     res.send({ success: false });
   }
 });
